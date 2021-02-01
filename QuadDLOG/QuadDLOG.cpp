96caf922f36003e081ecdf5e2711080b5fb1299e
ldsm8 <aleksei062@mail.ru>
2021-02-01 22:47:49 +0300

    init

---
+++ QuadDLOG/QuadDLOG.cpp
@@ -0,0 +1,28 @@
+﻿// QuadDLOG.cpp : Defines the entry point for the application.
+//
+
+#include "QuadDLOG.h"
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/math/special_functions/prime.hpp>
+#include <boost/multiprecision/miller_rabin.hpp>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	using namespace boost::multiprecision;
+	using namespace boost::random;
+	mt11213b base_gen(clock());
+	independent_bits_engine<mt11213b, 256, cpp_int> gen(base_gen);
+	mt19937 gen2(clock());
+	cpp_int n = gen();
+	cpp_int mybignum = 18446744073709551615ull;
+	//std::cout << mybignum * mybignum << std::endl;
+	std::cout << miller_rabin_test(7, 25, gen) << std::endl;
+	std::cout << miller_rabin_test(10, 25, gen);
+	//cpp_int x = boost::multiprecision::powm(1, 1, 1);
+	
+	return 0;
+}
