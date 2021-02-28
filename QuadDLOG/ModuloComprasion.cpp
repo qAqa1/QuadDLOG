c426d7f8bc95924fd2b4190699951a9a34f7d18e
ldsm8 <aleksei062@mail.ru>
2021-02-28 17:00:25 +0300

    return TonelliShanks

--- QuadDLOG/ModuloComprasion.cpp
+++ QuadDLOG/ModuloComprasion.cpp
@@ -4,7 +4,7 @@
 
 roots ModuloComprasion(int n, boost::multiprecision::cpp_int a, boost::multiprecision::cpp_int p)
 {
-    //if (n == 2) return TonelliShanks(a, p);
+    if (n == 2) return TonelliShanks(a, p);
     auto rts = CantorModularComprassion(n, a, p);
     return rts.empty() ? std::nullopt : std::make_optional(CantorModularComprassion(n, a, p));
 }
