edf4c37629aeb5fc2d711cb4159935a803bf6ea2
Aleksey <alelsei062@mail.ru>
2021-03-03 13:05:57 +0300

    small optimization

--- QuadDLOG/QuadraticResidueDLOG.cpp
+++ QuadDLOG/QuadraticResidueDLOG.cpp
@@ -7,106 +7,106 @@ using namespace Math;
 
 cpp_int ToRoot(cpp_int val, cpp_int twoPow)
 {
-    cpp_int two = 2;
-    //cpp_int twoPart = powm(two, twoPow, val);
-    cpp_int twoPart = Pow(two, twoPow);
-    auto primePart = FindBiggestPrimeNumInNum(twoPart, val);
-    std::cout << "Степень двойки: " << twoPart << " Простое число: " << primePart;
-    return twoPart * primePart;
+	cpp_int two = 2;
+	//cpp_int twoPart = powm(two, twoPow, val);
+	cpp_int twoPart = Pow(two, twoPow);
+	auto primePart = FindBiggestPrimeNumInNum(twoPart, val);
+	std::cout << "Степень двойки: " << twoPart << " Простое число: " << primePart;
+	return twoPart * primePart;
 }
 
 bool CheckRoot(cpp_int g, cpp_int n, cpp_int p, cpp_int root) { return powm(g, root, p) == n; }
 
 res_container CalcLevel(res_container n_values, cpp_int p, int degree)
 {
-    res_container next_level_data;
+	res_container next_level_data;
 
-    for (auto n : n_values)
-    {
-        //std::cout << std::endl << "p в CalcLevel = " << p << std::endl;
-        //std::cout << std::endl << "degree в CalcLevel = " << degree << std::endl;
-        auto res = ModuloComprasion(degree, n, p);
-        if (res)
-        {
-            auto roots = res.value();
-            for (const auto &root : roots)
-            {
-                next_level_data.push_back(root);
-            }        
-        }
-    }
+	for (auto& n : n_values)
+	{
+		//std::cout << std::endl << "p в CalcLevel = " << p << std::endl;
+		//std::cout << std::endl << "degree в CalcLevel = " << degree << std::endl;
+		auto res = ModuloComprasion(degree, n, p);
+		if (res)
+		{
+			auto roots = res.value();
+			for (const auto& root : roots)
+			{
+				next_level_data.push_back(root);
+			}
+		}
+	}
 
-    return next_level_data;
+	return next_level_data;
 }
 
 std::optional<boost::multiprecision::cpp_int> CalcDegree(int degree, cpp_int g, cpp_int n, cpp_int p)
 {
-    cpp_int level = 0;
-    level_data all_levels;
-    res_container levelData = { n };
+	cpp_int level = 0;
+	level_data all_levels;
+	res_container levelData = { n };
 
-    while (!levelData.empty())
-    {
-        //if (level > 6) return -1;
+	while (!levelData.empty())
+	{
+		//if (level > 6) return -1;
 
-        levelData = CalcLevel(levelData, p, degree);
+		levelData = CalcLevel(levelData, p, degree);
 
-        std::cout << "level = " << level++ << ":";
-        for (auto val : levelData)
-        {
-            std::cout << " " << val;
-        }
-        std::cout << std::endl;
+		std::cout << "level = " << level++ << ":";
+		for (auto& val : levelData)
+		{
+			std::cout << " " << val;
+		}
+		std::cout << std::endl;
 
-        auto it = std::find(std::begin(all_levels), std::end(all_levels), levelData);
-        if (it != std::end(all_levels))
-        {
-            std::cout << "Следующий уровень: ";
-            for (auto num : levelData) { std::cout << num << " "; }
-            std::cout << " является повторением уровня " << std::distance(std::begin(all_levels), it) + 1;
-            return std::nullopt;
-        }
+		auto it = std::find(std::begin(all_levels), std::end(all_levels), levelData);
+		if (it != std::end(all_levels))
+		{
+			std::cout << "Следующий уровень: ";
+			for (auto& num : levelData) { std::cout << num << " "; }
+			std::cout << " является повторением уровня " << std::distance(std::begin(all_levels), it) + 1;
+			return std::nullopt;
+		}
 
-        //if (std::find(std::begin(levelData), std::end(levelData), n) != std::end(levelData))
-        //{
-            //std::cout << "Содержит " << n << ". Проверка уровня:";
-            std::cout << "Проверка уровня:";
-            for (auto val : levelData)
-            {
-                std::cout << std::endl << "Значение: " << val << std::endl;
-                auto possibleRoot = ToRoot(val, level);
-                std::cout << std::endl << "Корень: " << possibleRoot;
-                auto checkResult = CheckRoot(g, n, p, possibleRoot);
-                std::cout << std::endl << "Проверка корня: " << (checkResult ? "Подходит" : "Не подходит");
-                if (checkResult) return std::make_optional(possibleRoot);
-                std::cout << std::endl << "--------------------------------";
-            }
+		//if (std::find(std::begin(levelData), std::end(levelData), n) != std::end(levelData))
+		//{
+			//std::cout << "Содержит " << n << ". Проверка уровня:";
+		std::cout << "Проверка уровня:";
+		for (auto& val : levelData)
+		{
+			std::cout << std::endl << "Значение: " << val << std::endl;
+			auto possibleRoot = ToRoot(val, level);
+			std::cout << std::endl << "Корень: " << possibleRoot;
+			auto checkResult = CheckRoot(g, n, p, possibleRoot);
+			std::cout << std::endl << "Проверка корня: " << (checkResult ? "Подходит" : "Не подходит");
+			if (checkResult) return std::make_optional(possibleRoot);
+			std::cout << std::endl << "--------------------------------";
+		}
 
-            std::cout << std::endl;
-        //}
+		std::cout << std::endl;
+		//}
 
-        all_levels.push_back(levelData);
-    }
+		all_levels.push_back(levelData);
+	}
 
-    std::cout << "Когда степень равна " << degree << ", при n = " << n << " и p = " << p << " нет корней";
-    return std::nullopt;
+	std::cout << "Когда степень равна " << degree << ", при n = " << n << " и p = " << p << " нет корней";
+	return std::nullopt;
 }
 
 std::optional<cpp_int> FindRoot(cpp_int g, cpp_int n, cpp_int p)
 {
-    std::cout << g << "^x = " << n << "(mod " << p << ")" << std::endl;
+	std::cout << g << "^x = " << n << "(mod " << p << ")" << std::endl;
 
-    int prime = 2;
+	int prime = 2;
 
-    while (true)
-    {
-        std::cout << "______________________________________________________" << std::endl;
-        std::cout << "Рассчет " << prime << " степени:" << std::endl;
-        auto possibleRoot = CalcDegree(prime, g, n, p);
-        if (possibleRoot) return possibleRoot;
-        prime = NextPrimeInt(prime);
-        std::cout << std::endl << std::endl;
-    }
+	while (true)
+	{
+		std::cout << "______________________________________________________" << std::endl;
+		std::cout << "Рассчет " << prime << " степени:" << std::endl;
+		auto possibleRoot = CalcDegree(prime, g, n, p);
+		if (possibleRoot) return possibleRoot;
+		prime = NextPrimeInt(prime);
+		std::cout << std::endl << std::endl;
+	}
 
-    //return CalcDegree(3, g, n, p);
+	//return CalcDegree(3, g, n, p);
 }
